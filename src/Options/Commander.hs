e78547f1bd37596b68d1f1b9dfde16bb149fd87c
Samuel Schlesinger <samuel@simspace.com>
2020-06-11 13:29:56 -0400

    Added some more middleware

--- src/Options/Commander.hs
+++ src/Options/Commander.hs
@@ -29,7 +29,7 @@ Portability: POSIX, Windows
 
 Commander is an embedded domain specific language describing a command line
 interface, along with ways to run those as real programs. An complete example
-of such a command line interface can be found as:
+of such a command line interface is:
 
 @
 main :: IO ()
@@ -48,8 +48,17 @@ main = command_ . toplevel @"file" $
       Nothing -> pure ())
 @
 
+If I run this program with the argument help, it will output:
+
+@
+usage:
+file maybe-read \<filename :: String\> ~read
+file maybe-write -file \<file-to-write :: String\>
+@
+
 The point of this library is mainly so that you can write command line
-interfaces quickly and easily, and not have to write any boilerplate.
+interfaces quickly and easily, with somewhat useful help messages, and 
+not have to write any boilerplate.
 -}
 module Options.Commander (
   -- ** Parsing Arguments and Options
@@ -73,7 +82,7 @@ module Options.Commander (
   -- ** The CommanderT Monad
   CommanderT(Action, Defeat, Victory), runCommanderT, initialState, State(State, arguments, options, flags),
   -- ** Middleware for CommanderT
-  Middleware, logState
+  Middleware, logState, transform, withActionEffects, withDefeatEffects, withVictoryEffects
 ) where
 
 import Control.Applicative (Alternative(..))
@@ -481,7 +490,7 @@ flag = FlagProgramT
 
 -- | A convenience combinator that constructs the program I often want
 -- to run out of a program I want to write.
-toplevel :: forall s p m a. (HasProgram p, KnownSymbol s, MonadIO m) 
+toplevel :: forall s p m. (HasProgram p, KnownSymbol s, MonadIO m) 
          => ProgramT p m () 
          -> ProgramT (Named s & ("help" & Raw + p)) m ()
 toplevel p = named (sub (usage @(Named s & ("help" & Raw + p))) <+> p)
@@ -495,14 +504,52 @@ infixr 2 <+>
 
 -- | A meta-combinator that takes a type-level description of a command 
 -- line program and produces a simple usage program.
-usage :: forall p m a. (MonadIO m, HasProgram p) => ProgramT Raw m ()
+usage :: forall p m. (MonadIO m, HasProgram p) => ProgramT Raw m ()
 usage = raw $ do
   liftIO $ putStrLn "usage:"
   void . traverse (liftIO . putStrLn . unpack) $ invocations @p
 
-type Middleware m = forall a. CommanderT State m a -> CommanderT State m a
+-- | The type of middleware, which can transform interpreted command line programs
+-- by meddling with arguments, options, or flags, or by adding effects for
+-- every step. You can also change the underlying monad.
+type Middleware m n = forall a. CommanderT State m a -> CommanderT State n a
 
-logState :: MonadIO m => Middleware m
+-- | Middleware to transform the base monad with a natural transformation.
+transform :: (Monad m, Monad n) => (forall a. m a -> n a) -> Middleware m n
+transform f commander = case commander of
+  Action a -> Action $ \state -> do
+    (commander', state') <- f (a state)
+    pure (transform f commander', state')
+  Defeat -> Defeat
+  Victory a -> Victory a 
+
+-- | Middleware to add monadic effects for every 'Action'. Useful for
+-- debugging complex command line programs.
+withActionEffects :: Monad m => m a -> Middleware m m
+withActionEffects ma = transform (ma *>)
+
+-- | Middleware to have effects whenever the program might backtrack.
+withDefeatEffects :: Monad m => m a -> Middleware m m
+withDefeatEffects ma commander = case commander of
+  Action a -> Action $ \state -> do
+    (commander', state') <- a state
+    pure (withDefeatEffects ma commander', state')
+  Defeat -> Action $ \state -> ma *> pure (Defeat, state)
+  Victory a -> Victory a
+
+-- | Middleware to have effects whenever the program successfully computes
+-- a result.
+withVictoryEffects :: Monad m => m a -> Middleware m m
+withVictoryEffects ma commander = case commander of
+  Action a -> Action $ \state -> do
+    (commander', state') <- a state
+    pure (withVictoryEffects ma commander', state')
+  Defeat -> Defeat
+  Victory a -> Action $ \state -> ma *> pure (Victory a, state)
+
+-- | Middleware to log the state to standard out for every step of the
+-- 'CommanderT' computation.
+logState :: MonadIO m => Middleware m m
 logState commander
   = case commander of
       Action a -> do
