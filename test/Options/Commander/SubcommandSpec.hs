d46657954757cf4e511bdb3bcbbb6ee4afdd0e36
William Rusnack <williamrusnack@gmail.com>
2020-12-19 17:34:17 -0500

    sub now has multi convergence

--- test/Options/Commander/SubcommandSpec.hs
+++ test/Options/Commander/SubcommandSpec.hs
@@ -5,7 +5,7 @@ import Test
 
 spec :: Spec
 spec = do
-  let program ::  ProgramT (Sub "a" & Raw + Sub "b" & Raw) IO (Either Int String)
+  let program ::  ProgramT (Sub '["a"] & Raw + Sub '["b"] & Raw) IO (Either Int String)
       program = (sub $ raw $ pure $ Left 1) <+> (sub $ raw $ pure $ Right "abc")
   testProgram "first  sub command" program (Just $ Left 1)      ["a"]
   testProgram "second sub command" program (Just $ Right "abc") ["b"]
